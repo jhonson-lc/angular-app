03c639d22d0934c063bde595bd4f7dd23ab7f22e
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-21 17:08:01 -0500

    feat(apps/app-gsi): Use components on app

--- apps/app-gsi/src/app/app.module.ts
+++ apps/app-gsi/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
