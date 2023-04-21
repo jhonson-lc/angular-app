d80137238b749f5cccf9264f1cd301e45c66626d
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-21 17:07:13 -0500

    refactor(apps/app-gsi): Add component of login

--- apps/app-gsi/src/app/core/components/login/login.component.ts
+++ apps/app-gsi/src/app/core/components/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'gsi-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  hidePassword = true;
 }
