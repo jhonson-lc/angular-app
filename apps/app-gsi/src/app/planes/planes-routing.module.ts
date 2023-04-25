7a1748b69e70295805133f11b66c681106219110
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-24 20:36:59 -0500

    feat(apps/app-gsi): Add routing

--- apps/app-gsi/src/app/planes/planes-routing.module.ts
+++ apps/app-gsi/src/app/planes/planes-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageListComponent } from './pages/page-list/page-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PageListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
