9fb3eb7094f186bbe41faa854789364a42976393
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-24 20:34:12 -0500

    feat(apps/app-gsi): Add title with route

--- apps/app-gsi/src/app/shared/components/title/title.component.ts
+++ apps/app-gsi/src/app/shared/components/title/title.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IMenu, MenuService } from 'src/app/helper/services/menu.service';
 
 @Component({
-  selector: 'app-title',
+  selector: 'gsi-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
 export class TitleComponent {
+  path: IMenu
 
+  constructor(private menuServicio: MenuService,
+    private activitedRoute: ActivatedRoute) {
+      const urlActive = '/' + activitedRoute.snapshot.pathFromRoot[1].routeConfig?.path;
+      this.path = menuServicio.getMenuByURL(urlActive);
+  }
 }
