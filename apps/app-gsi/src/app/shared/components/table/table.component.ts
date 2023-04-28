45410edefef301cb214a3d98d7172fbd9a7a1fcb
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-27 20:06:31 -0500

    feat(apps/app-gsi): Add table

--- apps/app-gsi/src/app/shared/components/table/table.component.ts
+++ apps/app-gsi/src/app/shared/components/table/table.component.ts
@@ -1,5 +1,6 @@
-import { Component, ContentChildren, Input, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
-import { MatColumnDef } from '@angular/material/table';
+import { Component, ContentChildren, Input, QueryList, SimpleChanges, ViewChild} from '@angular/core';
+
+import { MatColumnDef, MatTable } from '@angular/material/table';
 import { MetadataColumn } from 'src/app/interfaces/metadatacolumn.interface';
 
 @Component({
@@ -14,10 +15,18 @@ export class TableComponent {
   columns: string[] = []
 
   @ContentChildren(MatColumnDef, {descendants: true}) columnDef!:QueryList<MatColumnDef>
+  @ViewChild(MatTable,{static:true}) table!:MatTable<any>
 
   ngOnChanges(changes:SimpleChanges){
     if(changes['metaDataColumns']){
       this.columns = this.metaDataColumns.map(column => column.field)
     }
+  }
+
+  ngAfterContentInit(){
+    if(!this.columnDef){return}
+    this.columnDef.forEach(columnDef => {
+      this.columns.push(columnDef.name)
+      this.table.addColumnDef(columnDef)})
   }
 }
