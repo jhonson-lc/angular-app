9327a8cf91b2c8d43be0aa53a54d1a0f035a49c6
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-28 20:31:51 -0500

    feat(apps/app-gsi): Add keypadbuttons

--- apps/app-gsi/src/app/shared/components/keypad-button/keypad-button.component.ts
+++ apps/app-gsi/src/app/shared/components/keypad-button/keypad-button.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { KeypadButton } from 'src/app/interfaces/keypadbutton.interface';
 
 @Component({
-  selector: 'app-keypad-button',
+  selector: 'gsi-keypad-button',
   templateUrl: './keypad-button.component.html',
   styleUrls: ['./keypad-button.component.css']
 })
 export class KeypadButtonComponent {
+  @Input() keypadButtons: KeypadButton[] = []
+  @Output() onClick: EventEmitter<string> = new EventEmitter<string>();
 
+  enviarAccion(accion: string) {
+    this.onClick.emit(accion);
+  }
 }
