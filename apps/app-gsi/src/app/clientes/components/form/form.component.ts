57972be329d3e489e7e4008ad419763038eecf1a
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-28 20:32:47 -0500

    feat(apps/app-gsi): Add form

--- apps/app-gsi/src/app/clientes/components/form/form.component.ts
+++ apps/app-gsi/src/app/clientes/components/form/form.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { PageListComponent } from '../../pages/page-list/page-list.component';
 
 @Component({
-  selector: 'app-form',
+  selector: 'gsi-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+  titulo=""
+  formulario!: FormGroup
 
+  constructor(private referencia: MatDialogRef<FormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data:any){
+      this.titulo=data ? "EDICIÓN" : 'NUEVO'
+    }
+
+    ngOnInit(): void {
+      this.cargarFormulario()
+    }
+
+    grabar(){
+      this.referencia.close(this.formulario.value)
+    }
+
+    cargarFormulario(){
+      this.formulario = new FormGroup({
+        id: new FormControl(this.data?._id),
+        nombresCompletos: new FormControl(this.data?.nombresCompletos,Validators.required),
+        fechadeNacimiento: new FormControl(this.data?.fechadeNacimiento,Validators.required),
+        direccion: new FormControl(this.data?.direccion,Validators.required),
+        celular: new FormControl(this.data?.celular),
+      })
+    }
 }
