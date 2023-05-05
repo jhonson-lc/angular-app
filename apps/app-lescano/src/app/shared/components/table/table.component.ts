af5209060fb218e0538dfe5ba6292f3419008c7e
jhonson-lc <jhoncisneros11@gmail.com>
2023-05-04 23:12:52 -0500

    feat(apps/app-lescano): Add table component

---
+++ apps/app-lescano/src/app/shared/components/table/table.component.ts
@@ -0,0 +1,32 @@
+import { Component, ContentChildren, Input, QueryList, SimpleChanges, ViewChild} from '@angular/core';
+
+import { MatColumnDef, MatTable } from '@angular/material/table';
+import { MetadataColumn } from 'src/app/interfaces/metadatacolumn.interface';
+
+@Component({
+  selector: 'gsi-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+
+export class TableComponent {
+  @Input() data: any
+  @Input() metaDataColumns!: MetadataColumn[]
+  columns: string[] = []
+
+  @ContentChildren(MatColumnDef, {descendants: true}) columnDef!:QueryList<MatColumnDef>
+  @ViewChild(MatTable,{static:true}) table!:MatTable<any>
+
+  ngOnChanges(changes:SimpleChanges){
+    if(changes['metaDataColumns']){
+      this.columns = this.metaDataColumns.map(column => column.field)
+    }
+  }
+
+  ngAfterContentInit(){
+    if(!this.columnDef){return}
+    this.columnDef.forEach(columnDef => {
+      this.columns.push(columnDef.name)
+      this.table.addColumnDef(columnDef)})
+  }
+}
