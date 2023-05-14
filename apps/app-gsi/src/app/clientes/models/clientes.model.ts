045b39df7350a4cf6e1be0cd6a5942cc0f323a8f
jhonson-lc <jhoncisneros11@gmail.com>
2023-05-13 23:12:42 -0500

    feat(apps/app-gsi): Add model client

---
+++ apps/app-gsi/src/app/clientes/models/clientes.model.ts
@@ -0,0 +1,19 @@
+export class Cliente{
+  _id?: string;
+  nombresCompletos: string;
+  direccion: string;
+  fechadeNacimiento: Date;
+  celular: string;
+
+  constructor(
+    nombresCompletos: string,
+    direccion: string,
+    fechadeNacimiento: Date,
+    celular: string
+  ){
+    this.nombresCompletos = nombresCompletos;
+    this.direccion = direccion;
+    this.fechadeNacimiento = fechadeNacimiento;
+    this.celular = celular;
+  }
+}
