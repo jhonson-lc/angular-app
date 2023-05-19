19eb62395e218fbfb844d2e01576109a10e7dda1
jhonson-lc <jhoncisneros11@gmail.com>
2023-05-19 17:25:29 -0500

    feat(apps/app-gsi): Add page of reports

---
+++ apps/app-gsi/src/app/reportes/pages/page-reports/page-reports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-reports',
+  templateUrl: './page-reports.component.html',
+  styleUrls: ['./page-reports.component.css']
+})
+export class PageReportsComponent {
+
+}
